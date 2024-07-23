b680cf0c8b3f8f7d770a39398640ed5bde4cb784
dotiendat <mrdotiendat1992>
2024-07-23 13:04:34 +0700

    init

--- app.py
+++ app.py
@@ -460,46 +460,48 @@ def xoasanluongcanhan():
 @app.route("/taidulieuxuong", methods=["GET"])
 def taidulieuxuong():
     if request.method == "GET":
-        chuyen = request.args.get("chuyen")
-        ngay = request.args.get("ngay")
-        style = request.args.get("style")
-        rows = lay_danhsach_sanluong(ngay, chuyen, style,None,None,None)
-        data = []
-        for row in rows:
-            data.append({
-                "Mã số thẻ": int(row[0]),
-                "Họ tên": row[1],
-                "Chuyền": row[2],
-                "Ngày": row[3],
-                "Style": row[4],
-                "Mã công đoạn": int(row[5]) if row[5] else 0,
-                "Sản lượng cá nhân": int(row[6]) if row[6] else 0,
-            })
-        data_frame = DataFrame(data)
-        thoigian = datetime.datetime.now().strftime("%d%m%Y_%H%M%S")
-        excel_path = os.path.join(os.getcwd(),f"taixuong/data_{thoigian}.xlsx")
-        data_frame.to_excel(excel_path, index=False)
-        # Mở tệp Excel để chỉnh độ rộng cột
-        wb = load_workbook(excel_path)
-        ws = wb.active
+        try:
+            chuyen = request.args.get("chuyen")
+            ngay = request.args.get("ngay")
+            style = request.args.get("style")
+            rows = lay_danhsach_sanluong(ngay, chuyen, style,None,None,None)
+            data = []
+            for row in rows:
+                data.append({
+                    "Mã số thẻ": int(row[0]),
+                    "Họ tên": row[1],
+                    "Chuyền": row[2],
+                    "Ngày": row[3],
+                    "Style": row[4],
+                    "Mã công đoạn": int(row[5]) if row[5] else 0,
+                    "Sản lượng cá nhân": int(row[6]) if row[6] else 0,
+                })
+            data_frame = DataFrame(data)
+            thoigian = datetime.datetime.now().strftime("%d%m%Y_%H%M%S")
+            excel_path = os.path.join(os.getcwd(),f"taixuong/data_{thoigian}.xlsx")
+            data_frame.to_excel(excel_path, index=False)
+            # Mở tệp Excel để chỉnh độ rộng cột
+            wb = load_workbook(excel_path)
+            ws = wb.active
 
-        # Chỉnh độ rộng cột theo độ rộng dữ liệu
-        for column in ws.columns:
-            max_length = 0
-            column_letter = column[0].column_letter  # Lấy tên cột (ví dụ: 'A', 'B')
-            for cell in column:
-                try:
-                    if len(str(cell.value)) > max_length:
-                        max_length = len(cell.value)
-                except:
-                    pass
-            adjusted_width = max_length + 2
-            ws.column_dimensions[column_letter].width = adjusted_width
+            # Chỉnh độ rộng cột theo độ rộng dữ liệu
+            for column in ws.columns:
+                max_length = 0
+                column_letter = column[0].column_letter  # Lấy tên cột (ví dụ: 'A', 'B')
+                for cell in column:
+                    try:
+                        if len(str(cell.value)) > max_length:
+                            max_length = len(cell.value)
+                    except:
+                        pass
+                adjusted_width = max_length + 2
+                ws.column_dimensions[column_letter].width = adjusted_width
 
-        # Lưu lại tệp Excel đã chỉnh sửa
-        wb.save(excel_path)
-        return send_file(excel_path, as_attachment=True)
-
+            # Lưu lại tệp Excel đã chỉnh sửa
+            wb.save(excel_path)
+            return send_file(excel_path, as_attachment=True)
+        except Exception as e:
+            return str(e)
 @app.route("/taidulieulen", methods=["POST"])
 def taidulieulen():
     if request.method == "POST":
